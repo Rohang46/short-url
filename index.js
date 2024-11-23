945f8179244cee81a0f68dc854410b8bb12190a5
Rohan Gaikwad <gaikwadrohan0070@gmail.com>
2024-11-23 16:11:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express= require("express");
+const {connectToMongoDB}=require("./connect");
+const urlRoute =require('./routes/url');
+const app =express();
+
+const PORT =8000;
+
+connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
+.then(()=>console.log('MongoDb connected'))
+
+app.use(express.json());
+
+app.use("/url",urlRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
